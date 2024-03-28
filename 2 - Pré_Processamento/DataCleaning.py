2182f279614a8769d9c5965a6845649cb10b5308
CaioMoraes38 <caio.moraes.santos@gmail.com>
2024-03-28 06:43:09 -0300

    quinto

--- 2 - Pré_Processamento/DataCleaning.py
+++ 2 - Pré_Processamento/DataCleaning.py
@@ -4,48 +4,6 @@ import matplotlib.pyplot as plt    # Biblioteca para criação de gráficos
 import seaborn as sns   # Biblioteca para visualização de dados
 import matplotlib.font_manager as fm
 
-def create_bins(df, column_name, bins, labels):
-    df[column_name + '_bins'] = pd.cut(df[column_name], bins=bins, labels=labels)
-    return df
-
-def PlotBarChart(df, column_name_num):
-    """
-    Plota um gráfico de contagem das faixas de idade no dataframe df.
-    """  
-    # Verificar valores ausentes no dataframe
-    if df.isnull().values.any():
-        print("Atenção: existem valores ausentes no dataframe.")
-        
-    
-    # Definir propriedades de fonte
-    title_font = fm.FontProperties(weight='bold', size=20)
-
-    # Set the plot title, x-axis limit and show the plot
-    plt.title(f'No Stroke vs Stroke by {column_name_num}',fontproperties=title_font)
-    plt.xlabel(column_name_num)
-    plt.legend()
-    plt.grid(True)
-   
-     
-def PlotPieChart(df, column_name_cat):
-    # Crie um gráfico em formato de pizza com a contagem de cada fruta
-    fatias, texto, autotexto = plt.pie(df[column_name_cat].value_counts(), autopct='%.2f%%')
-
-    # Definir propriedades de fonte
-    title_font = fm.FontProperties(weight='bold', size=20)
-
-    # Adicione rótulos nas fatias
-    for fatia in fatias:
-        fatia.set_label('{}'.format(fatia.get_label()))
-
-    # Adicione legendas
-    plt.legend(fatias, df[column_name_cat].unique(), loc='best')
-
-    # Adicione título e formatação final
-    plt.title(f'Distribution of {column_name_cat}',fontproperties=title_font)
-    plt.axis('equal')
-    plt.tight_layout()
-    plt.savefig(f"previsao_AVC/9-Graphics/{column_name_cat}_pizza.png")
     
 def main():
     names = ["work_year","job_title","job_category","salary_currency","salary","salary_in_usd",
@@ -58,9 +16,13 @@ def main():
                      names = names,      # Nome das colunas 
                      usecols = features, # Define as colunas que serão  utilizadas
                      na_values='?')      # Define que ? será considerado valores ausentes
-
-   
+                     
+   # Imprime a quantidade de valores faltantes por coluna
+    print("VALORES FALTANTES\n")
+    print(df.isnull().sum())
+    print("\n")   
     print(df.describe().T)
+    df.to_csv(output_file, header=False, index=False)
     
 
     print(df.describe(include=object).T)
@@ -85,33 +47,5 @@ def main():
 
        
         print(df_contagem)
-    cat = []
-    num = []
-    for i in df.columns:
-        if df[i].dtypes == object:
-            cat.append(i)
-        else :
-            num.append(i)
-    for column_name_cat in cat:
-        PlotPieChart(df, column_name_cat)  
-    bins = []
-    labels = []
-    for column_name_num in num:   
-        # Criar faixas de 
-        if column_name_num == "age":
-            bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-            labels = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90"]
-        # Criar faixas de indice de massa corporal
-        elif column_name_num == "bmi":
-            bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-            labels = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100"]
-        # Criar faixas de avg_glucose_level
-        elif column_name_num == "avg_glucose_level":
-            bins = [0, 50, 100, 150, 200, 250, 300]
-            labels = ["0-50", "50-100", "100-150", "150-200", "200-250", "250-300"]
-
-        df = create_bins(df, column_name_num, bins, labels)     
-        PlotBarChart(df, column_name_num)
-    
 if __name__ == "__main__":
     main()
