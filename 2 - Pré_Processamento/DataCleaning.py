1a33457466d0570cbcb82196db1b7f26e37a8a41
CaioMoraes38 <caio.moraes.santos@gmail.com>
2024-03-28 07:48:45 -0300

    QUINTO

--- 2 - Pré_Processamento/DataCleaning.py
+++ 2 - Pré_Processamento/DataCleaning.py
@@ -1,48 +1,58 @@
 import pandas as pd     # Biblioteca para manipulação de dados em formato tabular
-import numpy as np      # Biblioteca para cálculos numéricos
-import matplotlib.pyplot as plt    # Biblioteca para criação de gráficos
-import seaborn as sns   # Biblioteca para visualização de dados
-import matplotlib.font_manager as fm
 
-    
 def main():
+    # Definição dos nomes das colunas e características do arquivo
     names = ["work_year","job_title","job_category","salary_currency","salary","salary_in_usd",
-             "employee_residence","experience_level","employment_type","work_setting","company_location"
-             ,"company_size"] 
+             "employee_residence","experience_level","employment_type","work_setting","company_location",
+             "company_size"] 
     features = ['work_year','job_title','salary_currency','salary','company_location','company_size']
-    output_file ='1-DataBase/DatasetJobsScienceDadosClear.csv'
+    original_output_file ='1-DataBase/DatasetJobsScienceDadosClear.csv'  
+    new_output_file ='1-DataBase/Dataset_Dados_Numericos.csv'    
     input_file ='1-DataBase/DatasetJobsScienceDados.csv'
-    df = pd.read_csv(input_file,         # Nome do arquivo com dados
-                     names = names,      # Nome das colunas 
-                     usecols = features, # Define as colunas que serão  utilizadas
-                     na_values='?')      # Define que ? será considerado valores ausentes
-                     
-   # Imprime a quantidade de valores faltantes por coluna
-    print("\nVALORES FALTANTES\n")
-    print(df.isnull().sum())
-    print("\n")   
-    print(df.describe().T)
 
-  
-    df.to_csv(output_file, header=False, index=False)   
-    
-    '''
-    Mostrar todos os valores e frequências de uma coluna categórica  
-    Para fazer isso, você pode seguir o exemplo abaixo:
-    '''
-    # 1. Escolha a coluna que você deseja examinar
-    for coluna in features:
-
-        # 2.método value_counts() para contar o número de ocorrências 
-        contagem = df[coluna].value_counts()
+    try:
+        # Leitura do arquivo CSV
+        df = pd.read_csv(input_file,         # Nome do arquivo com dados
+                         names = names,      # Nome das colunas 
+                         usecols = features, # Define as colunas que serão utilizadas
+                         na_values='?')      # Define que ? será considerado valores ausentes
+        
+        # Imprime a quantidade de valores faltantes por coluna
+        print("\nVALORES FALTANTES\n")
+        print(df.isnull().sum())
+        print("\n")   
 
       
-        df_contagem = contagem.to_frame().reset_index()
+        print(df.describe().T)
 
-        # 4. Renomeie as colunas para refletir o que elas representam
-        df_contagem.columns = [coluna, "Incidencia"]
+        print("\nDADOS CATEGÓRICOS PARA VALORES NUMÉRICOS\n")   
+
+        
+        for coluna in features:
+            contagem = df[coluna].value_counts()    
+            df_contagem = contagem.to_frame().reset_index()   
+            df_contagem.columns = [coluna, "Incidencia"]    
+            print(df_contagem)    
+
+      
+        df_numerical = df.copy()
+        df_numerical['work_year'] = df_numerical['work_year'].replace({'2020': 0, '2021': 1, '2022': 2,'2023':3})
 
        
-        print(df_contagem)
+        ShowInformationDataFrame(df_numerical, "\nDataframe Dados Numéricos")           
+        df.to_csv(original_output_file, header=False, index=False)  
+        df_numerical.to_csv(new_output_file, header=False, index=False)  
+        
+        print("Arquivos salvos com sucesso:", original_output_file, new_output_file)
+        
+    except Exception as e:
+        print("Ocorreu um erro ao salvar os arquivos:", e)
+
+def ShowInformationDataFrame(df, description):
+    print(description)
+    print(df.info())
+    print(df.describe())
+    print("\n")
+
 if __name__ == "__main__":
     main()
