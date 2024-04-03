754a9fe5f4d0c1fb6756155e3fb852e5fffb4057
CaioMoraes38 <caio.moraes.santos@gmail.com>
2024-04-03 19:20:10 -0300

    Sexto Commit

--- 1-Preprocessing/2-Skewness_Mean_Median_Mode.py
+++ 1-Preprocessing/2-Skewness_Mean_Median_Mode.py
@@ -1,29 +1,56 @@
 import pandas as pd
-import numpy as np
-import statistics as st
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, Normalizer
 
+def ShowInformationDataFrame(df, name):
+    print(f"{name}:")
+    print(df.head())
+    print(df.describe())
+    print("")
 
 def main():
-    df_clear = pd.read_csv('0-Datasets/DatasetJobsScienceDadosClear.csv')
-    print(df_clear.head(15))
+    input_file = '0-Datasets/DatasetJobsScienceDadosClear.csv'
+    
 
-    cat = df_clear.select_dtypes(include='object')
-    cat.columns
-    num = df_clear.select_dtypes(include=np.number)
-    num.columns
+    df = pd.read_csv(input_file, header=0)  
+    numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns
 
-    print("\nMedia Salarial em Dolares Americanos ao Ano:\n")
+    # Separando os recursos e o alvo
+    features = numeric_columns[:-1].tolist()
+    target = [numeric_columns[-1]]
 
-    Media = df_clear['salary_in_usd'].mean()
-    print('\nA média da coluna "salary_in_usd" é:', Media)
-    Assimetria = df_clear["salary_in_usd"].skew()
-    print('\nAssimetria da coluna "salary_in_usd" é:',Assimetria)
-    Moda = df_clear['salary_in_usd'].mode()
-    print('\nModa da coluna "salary_in_usd" é:',Moda)
+    ShowInformationDataFrame(df, "Dataframe Original")
 
-    for i in cat.columns:
-        print("Valor mais frequente na coluna " + i.upper() + " é: " + str(cat[i].mode()[0]))
+    x = df.loc[:, features].values
+    y = df.loc[:, target].values
 
+    x_zscore = StandardScaler().fit_transform(x)
+    normalized1_df = pd.DataFrame(data=x_zscore, columns=features)
+    normalized1_df = pd.concat([normalized1_df, df[target]], axis=1)
+    ShowInformationDataFrame(normalized1_df, "Dataframe Z-Score Normalized")
+
+    # Min-Max normalization
+    x_minmax = MinMaxScaler().fit_transform(x)
+    normalized2_df = pd.DataFrame(data=x_minmax, columns=features)
+    normalized2_df = pd.concat([normalized2_df, df[target]], axis=1)
+    ShowInformationDataFrame(normalized2_df, "Dataframe Min-Max Normalized")
+
+    # Robust Scaler
+    x_robust = RobustScaler().fit_transform(x)
+    normalized3_df = pd.DataFrame(data=x_robust, columns=features)
+    normalized3_df = pd.concat([normalized3_df, df[target]], axis=1)
+    ShowInformationDataFrame(normalized3_df, "Dataframe Robust Normalized")
+
+    # Unit Vector Transformation
+    x_norma = Normalizer().fit_transform(x)
+    normalized4_df = pd.DataFrame(data=x_norma, columns=features)
+    normalized4_df = pd.concat([normalized4_df, df[target]], axis=1)
+    ShowInformationDataFrame(normalized4_df, "Dataframe Unit Vector Normalized")
+    
+    # Loop para salvar cada dataframe separadamente
+    for method, normalized_df in [('Z-Score', normalized1_df), ('Min-Max', normalized2_df),
+                                ('Robust', normalized3_df), ('Unit-Vector', normalized3_df)]:
+        output_file = f'0-Datasets/Normatização_{method}_{input_file.split("/")[-1]}'
+        normalized_df.to_csv(output_file, header=False, index=False)
 
 if __name__ == "__main__":
     main()
--- 1-Preprocessing/4-DataNormalization.py
+++ 1-Preprocessing/4-DataNormalization.py
@@ -4,22 +4,20 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 def main():
-    # Carregar o DataFrame
+    
     df_clear = pd.read_csv('0-Datasets/DatasetJobsScienceDadosClear.csv')
     print(df_clear.head(15))
 
-    # Selecionar apenas as colunas numéricas
+   
     numeric_columns = df_clear.select_dtypes(include=[np.number]).columns
 
-    # Normalizar apenas as colunas numéricas usando Min-Max
+    
     scaler = MinMaxScaler()
     df_clear[numeric_columns] = scaler.fit_transform(df_clear[numeric_columns])
 
-    # Salvar o DataFrame normalizado em um arquivo CSV
+    
     df_clear.to_csv('0-Datasets/DatasetJobsScienceDadosClear_Normalized.csv', index=False)
 
-    # Plotar histogramas das colunas normalizadas
-    plt.figure(figsize=(8, 6))
     plt.hist(df_clear['work_year'], bins=20, color='skyblue', edgecolor='black')
     plt.title('Histograma do Ano de Trabalho Normalizado (Min-Max)')
     plt.xlabel('Ano de Trabalho Normalizado')
